8cdc3e41a4bc18cc7af9fb1a49881ae6216cc8e4
fengxiaodong <1578588291@qq.com>
2020-04-03 16:04:56 +0800

    feat:修复deployment重启

--- deployment_restart.py
+++ deployment_restart.py
@@ -7,9 +7,9 @@ def restart_deployment(deployment):
     #定义一些文件头
     headers = {'accept':'application/json'}
     #请求服务,例如：www.baidu.com
-    hostServer="10.210.110.224:8080"
+    hostServer="10.56.196.23:8080"
     #接口
-    requrl ="/openapi/v1/gateway/action/restart_deployment?deployment="+deployment+"&namespace=hq-namespace&cluster=yxops-test-k8s&apikey=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOjEwLCJleHAiOjE1ODQ0MzY5MjIsImlhdCI6MTU4NDQzNjkyMiwiaXNzIjoid2F5bmUifQ.NIWkkznWlxY8zgCA0am25VoXXCQHznVd7LlpIVX9utkaJFnEKi_6OMkWIIcwCjRKFx7WHVy1b_mZwfpxLZJecA"
+    requrl ="/openapi/v1/gateway/action/restart_deployment?deployment="+deployment+"&namespace=hq-system&cluster=yx-k8s&apikey=eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOjQsImV4cCI6MTU4NDYwMzIyMiwiaWF0IjoxNTg0NjAzMjIyLCJpc3MiOiJ3YXluZSJ9.aVHdCkGkE7_kMK3ErH_91DujfKFmX43KhG_x22DcGIOj97gvxsp0CHUsRrTjzjn0ZhR9lvB-4F6NcpeqrB8oDw"
     #连接服务器       
     conn = httplib.HTTPConnection(hostServer)
     #发送请求       
@@ -24,7 +24,7 @@ def restart_deployment(deployment):
 
 def query_deployment():
     # 打开数据库连接
-    db = MySQLdb.connect("10.210.110.221", "test", "test123", "wayne", charset='utf8' )
+    db = MySQLdb.connect("10.56.196.13", "wayne", "V2F5bmVfeW91eGluMTIz", "wayne", charset='utf8' )
     # 使用cursor()方法获取操作游标 
     cursor = db.cursor()
     # 使用execute方法执行SQL语句
@@ -37,11 +37,16 @@ def query_deployment():
     for i in range(len(data)):
         deployment_list.append(data[i][0])
     #列表过滤
-    deployment_list.remove('statmonitor-deployment')
+    #print deployment_list
+    #deployment_list.remove('statmonitor-deployment')
     for deployment in deployment_list:
-        restart_deployment(deployment)
+        print deployment
+    #    restart_deployment(deployment)
     # 关闭数据库连接
     db.close()
 
 if __name__ == '__main__':
-    query_deployment()
+    #query_deployment()
+    for line in open("service.txt"):
+        restart_deployment(line.strip())
+
