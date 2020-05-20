8ba0f9dff46bf73c77ff5dbe2ad6e0f3b350deaf
ops_common <ruanquandong@youxin.com>
2020-05-20 15:17:27 +0800

    feat:整合notice和update description

--- deployNotice.go
+++ deployNotice.go
@@ -9,7 +9,14 @@ import (
 	"bytes"
 	"time"
 	"strings"
+	"database/sql"
+	_ "github.com/go-sql-driver/mysql"
 )
+
+const (
+	optoolsToken = "b3B0b29scw=="
+)
+
 type Containers struct {
 	Image string `json:"image"`
 }
@@ -110,6 +117,12 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 		}
 		content := title+"项目：" + resourceName + "\n >操作人：" + user + "\n >动作：" + action + "\n >空间：" + nameSpace + "\n >镜像版本：" + imageTag + "\n >状态：" + status + "\n >时间：" + createTime
 		wechatNotification(content)
+
+		//update wayne description and delete invalid deployment_template
+		if nameSpace == "hq-system" {
+			go updateDescription()
+			go deleteDeploymentTemplate()
+		}
 	} else if action == "OfflineDeployment" {
 		resourceName := webhook.Payload.Deployment.ResourceName
 		nameSpace := webhook.NameSpace.Name
@@ -133,8 +146,128 @@ func handleWebhook(w http.ResponseWriter, r *http.Request) {
 	} 
 }
 
+func handleWebhookOptools(w http.ResponseWriter, r *http.Request) {
+	token := r.FormValue("token")
+	//fmt.Println(token)
+	if token == optoolsToken {
+		//update wayne description and delete invalid deployment_template
+		go updateDescription()
+		go deleteDeploymentTemplate()
+	} else {
+		log.Println("The token is error! Please check")	
+	}
+}
+
+func checkErr(errMasg error) {
+        if errMasg != nil {
+                panic(errMasg)
+        }
+}
+
+func queryData(query *sql.Rows) map[int]map[string]string {
+        column, _ := query.Columns()              //读出查询出的列字段名
+        values := make([][]byte, len(column))     //values是每个列的值，这里获取到byte里
+        scans := make([]interface{}, len(column)) //因为每次查询出来的列是不定长的，用len(column)定住当次查询的长度
+        for i := range values {                   //让每一行数据都填充到[][]byte里面
+                scans[i] = &values[i]
+        }
+        results := make(map[int]map[string]string) //最后得到的map
+        i := 0
+        for query.Next() { //循环，让游标往下移动
+                if err := query.Scan(scans...); err != nil { //query.Scan查询出来的不定长值放到scans[i] = &values[i],也就是每行都放在values里
+                        fmt.Println(err)
+                        //return
+                }
+                row := make(map[string]string) //每行数据
+                for k, v := range values {     //每行数据是放在values里面，现在把它挪到row里
+                        key := column[k]
+                        row[key] = string(v)
+                }
+                results[i] = row //装入结果集中
+                i++
+        }
+        return results
+}
+
+func updateDescription() {
+        db, err := sql.Open("mysql", "wayne:V2F5bmVfeW91eGluMTIz@tcp(10.56.196.13:3306)/?charset=utf8") //第一个参数为驱动名
+        checkErr(err)
+	defer db.Close()
+
+        //query publish status
+        queryPublish := "select resource_id, template_id from wayne.publish_status where type=0;"
+        queryPublishRes, err := db.Query(queryPublish)
+        checkErr(err)
+	defer queryPublishRes.Close()
+
+        resData := queryData(queryPublishRes)
+        for _, v := range resData {
+		resourceID := v["resource_id"]
+		templateID := v["template_id"]
+		
+		//query template description
+		var templateDescription string
+		queryTemplate := "select description from wayne.deployment_template where id=" + templateID + ";"
+		queryTemplateRes, err := db.Query(queryTemplate)
+		checkErr(err)
+		defer queryTemplateRes.Close()
+
+		resTemplateData := queryData(queryTemplateRes)
+		for _, vT := range resTemplateData {
+			templateDescription = vT["description"]
+		}
+
+		//query app id from deployment
+		var appID string
+		queryDeployment := "select app_id from wayne.deployment where id=" + resourceID + ";"
+		queryDeploymentRes, err := db.Query(queryDeployment)
+		checkErr(err)
+		defer queryDeploymentRes.Close()
+
+		resDeploymentData := queryData(queryDeploymentRes)
+		for _, vD := range resDeploymentData {
+			appID = vD["app_id"]
+		}
+		
+		//judge app is hq or not
+		if len(appID) >= 1 {
+			var namespaceID string
+			queryApp := "select namespace_id from wayne.app where id=" + appID + ";"
+			queryAppRes, err := db.Query(queryApp)
+			checkErr(err)
+			defer queryAppRes.Close()
+
+			resAppData := queryData(queryAppRes)
+			for _, vA := range resAppData {
+				namespaceID = vA["namespace_id"]
+			}
+			if namespaceID == "4" {
+				//update the description of app from publish_status
+				updateSql := "update wayne.app set description='" + templateDescription + "' where id=" + appID + ";"
+				row , err := db.Query(updateSql)
+				checkErr(err)
+				defer row.Close()
+			}
+		}
+        }
+}
+
+func deleteDeploymentTemplate() {
+        db, err := sql.Open("mysql", "wayne:V2F5bmVfeW91eGluMTIz@tcp(10.56.196.13:3306)/?charset=utf8") //第一个参数为驱动名
+        checkErr(err)
+        defer db.Close()
+
+	//delete the template of deployment
+	deleteSql := "delete from wayne.deployment_template where deleted=1;"
+	row, err := db.Query(deleteSql)
+	checkErr(err)
+	defer row.Close()
+}
+
+
 func main() {
     log.Println("server started")
 	http.HandleFunc("/webhook", handleWebhook)
+	http.HandleFunc("/webhookOptools", handleWebhookOptools)
 	log.Fatal(http.ListenAndServe(":8089", nil))
 }
